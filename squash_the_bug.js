d6c608a31321a85f30d182d739f56066521c986a
Crew Spence <csspence123@gmail.com>
2020-01-16 09:47:58 -0600

    Corrected bugs in Code Wars toy problem.  Solution works.

--- squash_the_bug.js
+++ squash_the_bug.js
@@ -1,18 +1,19 @@
 /*
-Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. 
+Output should be the length of the longest word, as a number.
 
 There will only be one 'longest' word.
 */
 
-function findLongest(str) (
+const findLongest = (str) => {
   
   var spl = str.split(" ");
-  var longest = 0
+  var longest = 0;
   
-  for (var i = 0; i > spl.length; i+) (
-    if (spl(i).length > longest) {
-      longest = spl[i].length
-    )
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length;
     }
-    return longest
-)
+    }
+    return longest;
+  }
